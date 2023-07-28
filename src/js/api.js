853f7c4eab8a4934a5a323d7d6f5de0ce2de20f3
Vadim CRM <Vadim.CRM@qtbox.net>
2023-07-28 18:28:19 +0300

    v1.9

--- src/js/api.js
+++ src/js/api.js
@@ -3,19 +3,18 @@ import _ from 'lodash'
 const key = '38484298-a4f98b849ca0293483b2c27a9'
 
 export const searchInfo = (info, page) => {
-     const params = new URLSearchParams({
-          key: key,
-          q: info,
-          image_type: 'photo',
-          orientation: 'horizontal',
-          safesearch: true,
-          per_page: 40,
-          page: page
-     });
      return axios({
           method: 'get',
           baseURL: 'https://pixabay.com/api',
-          url: `?${params.toString()}`
+          params: {
+               key: key,
+               q: info,
+               image_type: 'photo',
+               orientation: 'horizontal',
+               safesearch: true,
+               per_page: 40,
+               page: page
+          }
      }).then((response) => {
           if (!response.data) {
                new Error(response.status);
@@ -27,4 +26,33 @@ export const searchInfo = (info, page) => {
           console.log(error);
      })
 }
+
+
+
+// export const searchInfo = (info, page) => {
+//      const params = new URLSearchParams({
+//           key: key,
+//           q: info,
+//           image_type: 'photo',
+//           orientation: 'horizontal',
+//           safesearch: true,
+//           per_page: 40,
+//           page: page
+//      });
+//      return axios({
+//           method: 'get',
+//           baseURL: 'https://pixabay.com/api',
+//           url: `?${params.toString()}`
+//      }).then((response) => {
+//           if (!response.data) {
+//                new Error(response.status);
+//           }
+//           else {
+//                return response;
+//           }
+//      }).catch(error => {
+//           console.log(error);
+//      })
+// }
+
 
